f4e72c39d22bcda9541945a65ad1391caa6bd5df
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2019-04-06 00:01:25 -0700

    Finished custom training walkthrough

--- Research and experimentation/Custom training/walkthrough/main.py
+++ Research and experimentation/Custom training/walkthrough/main.py
@@ -12,22 +12,18 @@ import tempfile
 import os
 import utils
 
-# Eager execution is enabled by default on TF2.0
-
+print("TensorFlow version: {}".format(tf.__version__))
+print("Eager execution: {}".format(tf.executing_eagerly()))
 train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
-train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url), origin=train_dataset_url)
+train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                           origin=train_dataset_url)
 print("Local copy of the dataset file: {}".format(train_dataset_fp))
-
-# Parse file
-# column order in CSV file
 column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 feature_names = column_names[:-1]
 label_name = column_names[-1]
 print("Features: {}".format(feature_names))
 print("Label: {}".format(label_name))
 class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
-
-# Create a dataset
 batch_size = 32
 train_dataset = tf.data.experimental.make_csv_dataset(
     train_dataset_fp,
@@ -36,77 +32,57 @@ train_dataset = tf.data.experimental.make_csv_dataset(
     label_name=label_name,
     num_epochs=1)
 features, labels = next(iter(train_dataset))
-
-# Plot clusters
 plt.scatter(features['petal_length'].numpy(),
             features['sepal_length'].numpy(),
             c=labels.numpy(),
             cmap='viridis')
-
 plt.xlabel("Petal length")
 plt.ylabel("Sepal length")
 plt.show()
 
-# Pack data into a vector.
 def pack_features_vector(features, labels):
-	"""Pack the features into a single array."""
-	features = tf.stack(list(features.values()), axis=1)
-	return features, labels
+    """Pack the features into a single array."""
+    features = tf.stack(list(features.values()), axis=1)
+    return features, labels
+
 train_dataset = train_dataset.map(pack_features_vector)
 features, labels = next(iter(train_dataset))
-#print(features[:5])
-
-# create the model
 model = tf.keras.Sequential([
-	tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
-	tf.keras.layers.Dense(10, activation=tf.nn.relu),
-	tf.keras.layers.Dense(3)
+    tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
+    tf.keras.layers.Dense(10, activation=tf.nn.relu),
+    tf.keras.layers.Dense(3)
 ])
-predictions = model(features)
-#print(predictions[:5])
-tf.nn.softmax(predictions[:5])
-print("Prediction: {}".format(tf.argmax(predictions, axis=1)))
-print("    Labels: {}".format(labels))
-
-# Training
+#predictions = model(features)
+#print("Prediction: {}".format(tf.argmax(predictions, axis=1)))
+#print("Labels: {}".format(labels))
 def loss(model, x, y):
-	y_ = model(x)
-	return tf.keras.losses.sparse_categorical_crossentropy(y, y_)
-
-l = loss(model, features, labels)
-print("Loss test: {}".format(l))
-# Gradient function
-# For v1 compatibility:
-if(False==hasattr(tf.train, 'GradientDescentOptimizer')):
-    tf.train=tf.compat.v1.train
+    y_ = model(x)
+    return tf.compat.v1.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+#l = loss(model, features, labels)
+#print("Loss test: {}".format(l))
 def grad(model, inputs, targets):
-	with tf.GradientTape() as tape:
-		loss_value = loss(model, inputs, targets)
-	return loss_value, tape.gradient(loss_value, model.trainable_variables)
-# Gradient descent
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+    with tf.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets)
+    return loss_value, tape.gradient(loss_value, model.trainable_variables)
+optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.01)
 global_step = tf.Variable(0)
 loss_value, grads = grad(model, features, labels)
-print("Step: {}, Initial Loss: {}".format(global_step.numpy(),
-                                          loss_value.numpy()))
+print("Step: {}, Initial Loss: {}".format(global_step.numpy(),loss_value.numpy()))
 optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step)
-print("Step: {}, Loss: {}".format(global_step.numpy(), loss(model, features, labels).numpy()))
+print("Step: {}, Loss: {}".format(global_step.numpy(),loss(model, features, labels).numpy()))
 
-# Training loop
+# Train the network
 train_loss_results = []
 train_accuracy_results = []
-
 num_epochs = 201
-
 for epoch in range(num_epochs):
     epoch_loss_avg = tf.metrics.Mean()
     epoch_accuracy = tf.metrics.Accuracy()
-  # Training loop - using batches of 32
+    # Training loop - using batches of 32
     for x, y in train_dataset:
         # Optimize the model
         loss_value, grads = grad(model, x, y)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables),
-                                    global_step)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables),global_step)
         # Track progress
         epoch_loss_avg(loss_value)  # add current batch loss
         # compare predicted label to actual label
@@ -114,8 +90,16 @@ for epoch in range(num_epochs):
     # end epoch
     train_loss_results.append(epoch_loss_avg.result())
     train_accuracy_results.append(epoch_accuracy.result())
-  
     if epoch % 50 == 0:
         print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                epoch_loss_avg.result(),
-                                                                epoch_accuracy.result()))
+                                                                    epoch_loss_avg.result(),
+                                                                    epoch_accuracy.result()))
+
+fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+fig.suptitle('Training Metrics')
+axes[0].set_ylabel("Loss", fontsize=14)
+axes[0].plot(train_loss_results)
+axes[1].set_ylabel("Accuracy", fontsize=14)
+axes[1].set_xlabel("Epoch", fontsize=14)
+axes[1].plot(train_accuracy_results)
+plt.show()
